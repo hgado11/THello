ea6c685a248298f52ec90e2aea81aecd75d0e1f6
Hassan Gado <hgado11@gmail.com>
2018-11-04 07:47:36 +0300

    create the project

---
+++ Thello/Thello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thello
+//
+//  Created by Hassan Gadou on 11/4/18.
+//  Copyright © 2018 Hassan Gadou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
